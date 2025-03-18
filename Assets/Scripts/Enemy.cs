de9e992d9517481dad3187571a6a4a7cd2257314
yun <dbsejrdid123@gmail.com>
2025-03-18 19:25:19 +0900

    Enemy Aniamtion, Player Level System

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,7 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class Enemy : MonoBehaviour
 {
@@ -7,18 +10,23 @@ public class Enemy : MonoBehaviour
     public float maxHealth;
     public RuntimeAnimatorController[] animatorCon;
     public Rigidbody2D target;
+    WaitForFixedUpdate wait;
 
     bool isLive;
 
     Rigidbody2D rigid;
+    Collider2D coll;
     Animator anim;
     SpriteRenderer spriteRenderer;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        
+        wait = new WaitForFixedUpdate();
 
         
     }
@@ -27,13 +35,18 @@ public class Enemy : MonoBehaviour
     {
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         isLive = true;
+        coll.enabled = true;
+        rigid.simulated = true;
+        spriteRenderer.sortingOrder = 2;
+        anim.SetBool("Dead",false);
         health = maxHealth;
+        
 
     }
 
     void FixedUpdate()
     {
-        if(!isLive) return;
+        if(!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit")) return;
 
         Vector2 dirVec = target.position - rigid.position; // 타켓 위치 - 나의 위치 = 방향 
         Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
@@ -63,19 +76,42 @@ public class Enemy : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(!collision.CompareTag("Bullet")) return;
+        if(!collision.CompareTag("Bullet") || !isLive) return;
 
         health -= collision.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
 
-        if(health < 0)
+        if(health > 0)
         {
-            Dead();
+            anim.SetTrigger("Hit");
+        }
+        else
+        {
+            isLive = false;
+            coll.enabled = false;
+            rigid.simulated = false;
+            spriteRenderer.sortingOrder = 1;
+            anim.SetBool("Dead",true);
+            GameManager.instance.Kill++;
+            GameManager.instance.GetExp();
         }
         
     }
 
+    IEnumerator KnockBack()
+    {
+        yield return wait; // 다음 하나의 물리 프레임 딜레이 
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+
+        rigid.AddForce(dirVec.normalized * 3,ForceMode2D.Impulse);
+
+
+    }
+
     void Dead()
     {
+        
         gameObject.SetActive(false);
 
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,49 @@
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [Header("# Game Control")]
     public float gameTime;
     public float maxGameTime = 20f;
+
+    [Header("# PlayerInfo")]
+    [SerializeField]
+    int _level;
+    [SerializeField]
+    int _kill;
+    [SerializeField]
+    int _exp;
+    public int[] nextExp = {3,5,10,100,150,210,280,500,600,1000,2000};
+
+
+    public int Exp
+    {
+        get 
+        { 
+           
+            return _exp;
+        }
+        set { _exp = value;}
+
+
+    }
+    public int Kill
+    {
+        get { return _kill;}    
+        set { _kill = value;}
+    }
+    public int Level
+    {
+        get { return _level;}  
+        set { _level = value;}
+
+    }
+
+
+    [Header("# GameObject")]
     public PoolManager pool;
     public Player player;
 
@@ -25,6 +63,16 @@ public class GameManager : MonoBehaviour
         }
        
         
+    }
+
+    public void GetExp()
+    {
+        Exp++;
+        if(Exp == nextExp[Level])
+        {
+            Level++;
+            Exp = 0;
+        }
     }
 
 }
